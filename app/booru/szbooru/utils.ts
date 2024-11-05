5623b75786ce8224a68e637e854b2afdcb9b727b
Kentai Radiquum <kentai.waah@gmail.com>
2024-11-05 22:08:36 +0500

    feat: start home page

--- app/booru/szbooru/utils.ts
+++ app/booru/szbooru/utils.ts
@@ -1,5 +1,5 @@
 // get info
-import { SZ_ENDPOINTS} from "./endpoints";
+import { SZ_ENDPOINTS } from "./endpoints";
 
 export async function SZ_GetInfo(HOST: string) {
   try {
@@ -20,4 +20,28 @@ export async function SZ_GetInfo(HOST: string) {
     console.error(err);
     return err;
   }
+}
+
+export function bytesToHuman(bytes: number, si = false, dp = 1) {
+  const thresh = si ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + " B";
+  }
+
+  const units = si
+    ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  let u = -1;
+  const r = 10 ** dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  );
+
+  return bytes.toFixed(dp) + " " + units[u];
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSettingsStore } from "./store/settings";
+import { SZ_GetInfo, bytesToHuman } from "./booru/szbooru/utils";
+import { SZ_TYPE_Info } from "./booru/szbooru/types/api/info";
 
 export default function Home() {
+  const settingsStore = useSettingsStore();
+
+  const [booruInfo, setBooruInfo] = useState<SZ_TYPE_Info | null>(null);
+
+  useEffect(() => {
+    const _getinfo = async () => {
+      setBooruInfo(
+        await SZ_GetInfo(
+          settingsStore.boorus[settingsStore.lastActiveBooru].host
+        )
+      );
+    };
+    _getinfo();
+  }, []);
+
   return (
-    <div>This is home page. <a href="/another-page">Another page</a></div>
+    <>
+      {!booruInfo ? (
+        <progress className="circle large center middle fixed"></progress>
+      ) : (
+        <main className="responsive middle-align center-align">
+          <div className="middle">
+            <h1>
+              {booruInfo.config.name}
+            </h1>
+            <div className="row center-align medium-space">
+              <p>{bytesToHuman(booruInfo.diskUsage)}</p>
+              <p>{booruInfo.postCount} posts</p>
+            </div>
+          </div>
+        </main>
+      )}
+    </>
   );
 }
